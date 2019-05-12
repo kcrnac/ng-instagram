9f36cda8aa3fab4ccd867f9d74e5a73775458be9
Kristijan Crnac <kcrnacch@gmail.com>
2019-05-12 11:31:51 +0200

    Remove example projects from solution folder

--- backend/Instagram.Repository.Model/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Instagram.Repository.Model
-{
-    public class Account : BaseEntity
-    {
-        public ApplicationUser User { get; set; }
-
-        public Settings.Settings Settings { get; set; }
-    }
-}
--- backend/Instagram.Repository.Model/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Instagram.Repository.Model
-{
-    public class ApplicationUser : IdentityUser<int>
-    {
-        public string ProfilePictureUrl { get; set; }
-    }
-}
