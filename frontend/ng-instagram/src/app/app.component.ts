8ed9657887fdb90e0c1af5de8b09dde04b32e5af
Kristijan <kcrnacch@gmail.com>
2019-05-27 16:52:17 +0200

    Add footer & header ng components

--- frontend/ng-instagram/src/app/app.component.ts
+++ frontend/ng-instagram/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-instagram';
+
+  constructor(private toastrService: ToastrService) {
+  }
+
+  ngOnInit() {
+    this.toastrService.error('Test error');
+  }
 }
--- frontend/ng-instagram/src/app/app.module.ts
+++ frontend/ng-instagram/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
